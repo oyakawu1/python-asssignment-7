e768650ca623d067e7c2201b28f8dd2ecc572f7e
oyakawu1 <eakawu2@gmail.com>
2025-09-02 10:55:59 +0100

    first commit

--- attendance.py
+++ attendance.py
@@ -1,42 +1,40 @@
-"""
-Attendance Register
-
-Task:
-- Track attendance of students.
-- Use a dictionary { "student_id": {"name": str, "present_days": list, "absent_days": list} }
-- Functions to mark attendance, check history, and get reports.
-- Use your head/logic to mark multiple students at once.
-- Use **kwargs for flexible reporting (e.g., only_present=True).
-
-// NOT FOR THIS ASSIGNMENT
-Future OOP Extension:
-- Student class with mark_present() and mark_absent().
-- AttendanceRegister class that manages records.
-"""
+# attendance.py
 
 import datetime
 
 attendance = {}
-
-def register_student(student_id, name):
+ID = {"student_id": 0}
+def register_student(student_id, *name):
     """Register a student in the system."""
-    pass
-
-def mark_present(student_ids):
+    for students in name:
+        ID["student_id"] += 1
+        attendance[ID["student_id"]] = {"name": students, "present_days": [], "absent_days": []}
+    print(attendance)
+register_student(ID["student_id"], "Jon", "Leo", "Kenan")
+def mark_present(*student_ids):
     """Mark multiple students as present for today."""
     today = str(datetime.date.today())
     # implement logic
-    pass
-
-def mark_absent(student_ids):
+    for ids in student_ids:
+        if ids in attendance:
+            attendance[ids]["present_days"].append(today)
+    print(attendance)
+mark_present(1, 2)
+def mark_absent(*student_ids):
     """Mark multiple students as absent for today."""
     today = str(datetime.date.today())
     #implement logic
-    pass
+    for ids in student_ids:
+        if ids in attendance:
+            attendance[ids]["absent_days"].append(today)
+    print(attendance)
+mark_absent(1, 2)
 
-def get_report(**kwargs):
+
+def get_report(SID = 1, **details):
     """Generate attendance report with optional filters."""
     report = {}
     # implement logic
-
+    report["REPORT"] = details
     return report
+print(get_report(Days_absent = attendance[1]["absent_days"], Days_present = attendance[1]["present_days"]))
--- banking.py
+++ banking.py
@@ -1,36 +1,50 @@
-"""
-Bank Account Simulation
 
-Task:
-- Manage simple bank accounts.
-- Store accounts in dictionary { "account_number": {"name": str, "balance": float} }
-- Deposit, withdraw, transfer between accounts.
-- Use *args for batch deposits/withdrawals.
-- Use **kwargs for flexible account creation (e.g., overdraft_limit).
-
-// NOT FOR THIS TASK
-Future OOP Extension:
-- BankAccount class with methods deposit(), withdraw(), transfer().
-- Bank class to manage all accounts.
-"""
+#Bank Account Simulation
 
 accounts = {}
 
-def create_account(account_number, name, **kwargs):
+def create_account(account_number, name, **details):
     """Create an account with optional features like overdraft_limit."""
-    pass
+    accounts[account_number] = {"name":name, "details": details }
+    print(accounts)
 
+create_account((len(accounts) + 18273), "John", Balance = 43224, Branch = "Jos")
 def deposit(account_number, amount):
     """Deposit money into account.
         return "Account not found!" (if account does not exists)
         return Deposited {amount} into {accounts name}'s account. if account exists
     """
-    pass
+    if account_number in accounts:
+        accounts[account_number]["details"]["Balance"] += amount
+        print(f"The sum of {amount:,.2f} was deposited into account: {account_number} successfully!")
+    else:
+        return "Account not found!"
+    print(accounts)
+print(deposit(18273, 10))
 
 def withdraw(account_number, amount):
     """Withdraw money if balance is sufficient. else: insufficient funds"""
-    pass
+    if account_number in accounts:
+        if amount <= accounts[account_number]["details"]["Balance"]:
+            accounts[account_number]["details"]["Balance"] -= amount
+        else:
+            return "Insufficicient funds!"
+    else:
+        return "Account not found!"
+    print(accounts)
+print(withdraw(18274, 20400))
 
 def transfer(from_acc, to_acc, amount):
     """Transfer money between accounts. if funds is sufficient"""
-    pass
+    if from_acc in accounts:
+        if amount <= accounts[from_acc]["details"]["Balance"]:
+            accounts[from_acc]["details"]["Balance"] -= amount
+            accounts[to_acc]["details"]["Balance"] += amount
+            print(f"The sum of {amount:,.2f} was transfered into account: {to_acc} from account: {from_acc}")
+
+        else:
+            return "Insufficicient funds!"
+    else:
+        return "Account not found!"
+    print(accounts)
+transfer(18274, 18273, 10000)
